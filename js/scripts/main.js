d330a1e985e1dcffe2a188fbc58bb7ab017c628d
Lucas da Silva <sklucassilva@gmail.com>
2023-01-18 18:46:00 -0300

    dropdown pokemons terminado

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,23 +1,31 @@
 // scripts do slide principal
 var slide_hero = new Swiper(".slide-hero", {
-    pagination: {
-      el: ".slide-hero .main-area .area-explore .swiper-pagination",
-    },
-  });
+  pagination: {
+    el: ".slide-hero .main-area .area-explore .swiper-pagination",
+  },
+});
 
 const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
 const btnCloseModal = document.querySelector('.js-close-modal-details-pokemon')
 
 function openDetailsPokemon() {
-    document.documentElement.classList.add('open-modal');
+  document.documentElement.classList.add('open-modal');
 }
 
 function closeDetailsPokemon() {
-    document.documentElement.classList.remove('open-modal');
+  document.documentElement.classList.remove('open-modal');
 }
 
 cardPokemon.forEach(card => {
-    card.addEventListener('click', openDetailsPokemon);
+  card.addEventListener('click', openDetailsPokemon);
 })
 
-btnCloseModal.addEventListener('click', closeDetailsPokemon);
+if (btnCloseModal) {
+  btnCloseModal.addEventListener('click', closeDetailsPokemon);
+}
+
+const btnDropdownSelect = document.querySelector('.js-open-select-custom')
+
+btnDropdownSelect.addEventListener('click', () => {
+  btnDropdownSelect.parentElement.classList.toggle('active')
+})
