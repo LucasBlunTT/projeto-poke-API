f0eac812c9c56e7794f1eb99b2973c0c689085ba
Lucas da Silva <sklucassilva@gmail.com>
2023-02-16 19:37:38 -0300

    Card pokemon criado via API com funcao de modal

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -8,14 +8,6 @@ var slide_hero = new Swiper(".slide-hero", {
 const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
 const btnCloseModal = document.querySelector('.js-close-modal-details-pokemon')
 
-function openDetailsPokemon() {
-  document.documentElement.classList.add('open-modal');
-}
-
-function closeDetailsPokemon() {
-  document.documentElement.classList.remove('open-modal');
-}
-
 cardPokemon.forEach(card => {
   card.addEventListener('click', openDetailsPokemon);
 })
@@ -24,42 +16,105 @@ if (btnCloseModal) {
   btnCloseModal.addEventListener('click', closeDetailsPokemon);
 }
 
-const btnDropdownSelect = document.querySelector('.js-open-select-custom')
+const btnDropdownSelect = document.querySelector('.js-open-select-custom');
 
 btnDropdownSelect.addEventListener('click', () => {
-  btnDropdownSelect.parentElement.classList.toggle('active')
+  btnDropdownSelect.parentElement.classList.toggle('active');
 })
+
+const areaPokemons = document.getElementById('js-list-pokemons');
+
+function primeiraLEtraMaiuscula(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function createCardPokemon(code, type, nome, imagePok) {
+  let card = document.createElement('button');
+  card.classList = `card-pokemon js-open-details-pokemon ${type}`;
+  areaPokemons.appendChild(card);
+
+  let image = document.createElement('div');
+  image.classList = 'image';
+  card.appendChild(image);
+
+  let imagemSrc = document.createElement('img');
+  imagemSrc.classList = 'thumb-img';
+  imagemSrc.setAttribute('src', imagePok);
+  image.appendChild(imagemSrc);
+
+  let infoCardPokemon = document.createElement('div');
+  infoCardPokemon.classList = 'info';
+  card.appendChild(infoCardPokemon);
+
+  let infoTextPokemon = document.createElement('div');
+  infoTextPokemon.classList = 'text';
+  infoCardPokemon.appendChild(infoTextPokemon);
+
+  let codePokemon = document.createElement('span');
+  codePokemon.textContent = (code < 10) ? `#00${code}` : (code < 100) ? `#0${code}` : `#${code}`;
+  infoTextPokemon.appendChild(codePokemon);
+
+  let namePokemon = document.createElement('h3');
+  namePokemon.textContent = primeiraLEtraMaiuscula(nome);
+  infoTextPokemon.appendChild(namePokemon);
+
+  let areaIcon = document.createElement('div');
+  areaIcon.classList = 'icon';
+  infoCardPokemon.appendChild(areaIcon);
+
+  let imgType = document.createElement('img');
+  imgType.setAttribute('src', `img/icon-types/${type}.svg`)
+  areaIcon.appendChild(imgType);
+}
 
 function listingPokemons(urlAPI) {
   axios({
     method: 'GET',
     url: urlAPI
   })
-  .then((response) => {
-    const countPokemons = document.getElementById('js-count-pokemons')
+    .then((response) => {
+      const countPokemons = document.getElementById('js-count-pokemons')
 
-    const { results, next, count } = response.data;
+      const { results, next, count } = response.data;
 
-    countPokemons.innerText = count;
+      countPokemons.innerText = count;
 
-    results.forEach(pokemon =>{
-      let urlApiDetails = pokemon.url;
+      results.forEach(pokemon => {
+        let urlApiDetails = pokemon.url;
 
-      axios({
-        method: 'GET',
-        url: `${urlApiDetails}`
-      })
-      .then(response => {
-        const {name, id, sprites, types } = response.data;
+        axios({
+          method: 'GET',
+          url: `${urlApiDetails}`
+        })
+          .then(response => {
+            const { name, id, sprites, types } = response.data;
 
-        const infoCard = {
-          nome: name,
-          code: id,
-          image: sprites.other.dream_world.front_default
-        }
+            const infoCard = {
+              nome: name,
+              code: id,
+              image: sprites.other.dream_world.front_default,
+              type: types[0].type.name
+            }
+
+            createCardPokemon(infoCard.code, infoCard.type, infoCard.nome, infoCard.image);
+
+            const cardPokemon = document.querySelectorAll('.js-open-details-pokemon')
+
+            cardPokemon.forEach(card => {
+              card.addEventListener('click', openDetailsPokemon);
+            });
+
+          })
       })
     })
-  })
 }
 
 listingPokemons('https://pokeapi.co/api/v2/pokemon?pokemon?limit=9&offset=0')
+
+function openDetailsPokemon() {
+  document.documentElement.classList.add('open-modal');
+}
+
+function closeDetailsPokemon() {
+  document.documentElement.classList.remove('open-modal');
+}
