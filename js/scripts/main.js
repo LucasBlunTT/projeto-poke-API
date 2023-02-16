04e47bbc9a522639b58c8cfab69c66f23215829c
Lucas da Silva <sklucassilva@gmail.com>
2023-02-16 17:52:59 -0300

    implementação do axios

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -29,3 +29,37 @@ const btnDropdownSelect = document.querySelector('.js-open-select-custom')
 btnDropdownSelect.addEventListener('click', () => {
   btnDropdownSelect.parentElement.classList.toggle('active')
 })
+
+function listingPokemons(urlAPI) {
+  axios({
+    method: 'GET',
+    url: urlAPI
+  })
+  .then((response) => {
+    const countPokemons = document.getElementById('js-count-pokemons')
+
+    const { results, next, count } = response.data;
+
+    countPokemons.innerText = count;
+
+    results.forEach(pokemon =>{
+      let urlApiDetails = pokemon.url;
+
+      axios({
+        method: 'GET',
+        url: `${urlApiDetails}`
+      })
+      .then(response => {
+        const {name, id, sprites, types } = response.data;
+
+        const infoCard = {
+          nome: name,
+          code: id,
+          image: sprites.other.dream_world.front_default
+        }
+      })
+    })
+  })
+}
+
+listingPokemons('https://pokeapi.co/api/v2/pokemon?pokemon?limit=9&offset=0')
